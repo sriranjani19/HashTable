8ea1221a80fff4a6263bb7a011a5079e2ce1b9de
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-05-18 10:05:26 +0530

    Add files via upload

---
+++ FindFrequency.cs
@@ -0,0 +1,40 @@
+﻿
+
+   
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;   
+namespace MyHashTable
+{
+    public class HashTable
+    {
+
+
+        public static void MyMapNode()
+        {
+            Hashtable HT = new Hashtable();
+            HT.Add(1, "TO BE");
+            HT.Add(2, "NOT TO BE");
+            foreach (object i in HT.Keys)
+                Console.WriteLine(i);
+            foreach (object J in HT.Values)
+                Console.WriteLine(J);
+            foreach (DictionaryEntry di in HT)
+                Console.WriteLine("keys={0} values={1}", di.Key, di.Value);
+            Console.ReadKey();
+
+        }
+
+        internal void Add(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Remove(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
