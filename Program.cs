145efd338b5aacb42c17946ce953635523bf4ee3
sriranjani19 <99411199+sriranjani19@users.noreply.github.com>
2022-05-18 10:04:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace MyHashTable // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocome to hash table");
+            HashTable.MyMapNode();
+            Frequency.FindIndexes();
+            MyHashTable.Node();
+
+
+
+
+
+        }
+
+
+    }
+}
